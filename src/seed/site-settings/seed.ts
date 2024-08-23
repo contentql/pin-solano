c0f2b7ddf8de5926c47343168a9fcd81d034d9ff
rahaman002 <36rahaman@gmail.com>
2024-08-23 11:35:31 +0530

    chore: update seed type

--- src/seed/site-settings/seed.ts
+++ src/seed/site-settings/seed.ts
@@ -65,7 +65,7 @@ const seed = async (): Promise<SiteSetting> => {
     }
 
     const result = await payload.updateGlobal({
-      data: formattedSiteSettingsData,
+      data: formattedSiteSettingsData as SiteSetting,
       slug: 'site-settings',
     })
 
