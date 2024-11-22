b961a834e8143e83b2ba7daab9fafa0bf3b6fa2b
charan <charanm927@gmail.com>
2024-11-22 13:03:02 +0530

    chore: add suspense

--- src/payload/blocks/RenderBlocks.tsx
+++ src/payload/blocks/RenderBlocks.tsx
@@ -3,7 +3,7 @@
 import { env } from '@env'
 import { Page } from '@payload-types'
 import { useLivePreview } from '@payloadcms/live-preview-react'
-import React from 'react'
+import React, { Suspense } from 'react'
 
 import { trpc } from '@/trpc/client'
 import { cn } from '@/utils/cn'
@@ -37,26 +37,28 @@ const RenderBlocks: React.FC<RenderBlocksProps> = ({
   const dataToUse = livePreviewData?.layout || pageData?.layout
 
   return (
-    <div>
-      {dataToUse?.map((block, index) => {
-        // Casting to 'React.FC<any>' to bypass TypeScript error related to 'Params' type incompatibility.
-        const Block = blocksJSX[block.blockType] as React.FC<any>
+    <Suspense fallback={null}>
+      <div>
+        {dataToUse?.map((block, index) => {
+          // Casting to 'React.FC<any>' to bypass TypeScript error related to 'Params' type incompatibility.
+          const Block = blocksJSX[block.blockType] as React.FC<any>
 
-        if (Block) {
-          return (
-            <div
-              key={index}
-              className={cn(
-                index % 2 === 0 ? 'bg-transparent' : 'bg-[#26304e]',
-              )}>
-              <Block {...block} params={params} />
-            </div>
-          )
-        }
+          if (Block) {
+            return (
+              <div
+                key={index}
+                className={cn(
+                  index % 2 === 0 ? 'bg-transparent' : 'bg-[#26304e]',
+                )}>
+                <Block {...block} params={params} />
+              </div>
+            )
+          }
 
-        return <h3 key={block.id}>Block does not exist </h3>
-      })}
-    </div>
+          return <h3 key={block.id}>Block does not exist </h3>
+        })}
+      </div>
+    </Suspense>
   )
 }
 
